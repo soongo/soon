925744204528c4588d8932d2742282912b61fce3
guoyao <guoyao.me@gmail.com>
2020-11-22 23:18:17 +0800

    Add `DevLog` built-in middleware for development

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/dlclark/regexp2 v1.2.0
+	github.com/fatih/color v1.10.0
 	github.com/go-playground/validator/v10 v10.3.0
 	github.com/soongo/negotiator v0.6.3
 	github.com/soongo/path-to-regexp v1.5.0
--- middlewares.go
+++ middlewares.go
@@ -5,8 +5,12 @@
 package soon
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"github.com/fatih/color"
 
 	"github.com/soongo/soon/renderer"
 	"github.com/soongo/soon/util"
@@ -41,5 +45,29 @@ func Static(root string, options ...renderer.FileOptions) Handle {
 		}
 
 		c.SendFile(absPath, options...)
+	}
+}
+
+// DevLog is a built-in middleware function in Soon.
+// It just print simple log for development.
+// For production environment, you can use `https://github.com/sirupsen/logrus`
+func DevLog() Handle {
+	g, r, y := color.New(color.FgGreen).SprintFunc(),
+		color.New(color.FgRed).SprintFunc(),
+		color.New(color.FgYellow).SprintFunc()
+
+	return func(c *Context) {
+		req, w := c.Request, c.Writer
+		method, path := req.Method, req.URL.String()
+		start := time.Now()
+		c.Next()
+		cost, status, size := time.Since(start), w.Status(), w.Size()
+		statusText := g(status)
+		if status >= 500 {
+			statusText = r(status)
+		} else if status >= 400 {
+			statusText = y(status)
+		}
+		fmt.Printf("%s %s %s %s - %d\n", method, path, statusText, cost, size)
 	}
 }
